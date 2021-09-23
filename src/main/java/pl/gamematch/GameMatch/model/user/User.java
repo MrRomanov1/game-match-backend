6661c8fbc0c3ecd85af5e9d6a31ec4c4d315520a
Piotr <piotr.romanczak@gmail.com>
2021-09-23 23:04:02 +0200

    GAM-1 GAM-3 added User and Game structure

---
+++ src/main/java/pl/gamematch/GameMatch/model/user/User.java
@@ -0,0 +1,59 @@
+package pl.gamematch.GameMatch.model.user;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Date;
+
+/**
+ * Created by Piotr Romanczak on 23-09-2021
+ * Description: User class
+ */
+
+@Entity
+@Table(name="user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_email")
+    private String email;
+
+    @Column(name = "user_password")
+    private Double password;
+
+    @Column(name = "user_first_name")
+    private String firstName;
+
+    @Column(name = "user_last_name")
+    private String lastName;
+
+    @CreatedDate
+    @Column(name = "created_date")
+    private Date createdDate;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = {@JoinColumn(name = "user_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
+    private Collection<Role> roles;
+
+    /**
+     * Created by Piotr Romanczak on 23-09-2021
+     * Description: This method is responsible to return full user name
+     * @return String fullName
+     */
+
+    public String getFullName() {
+        return firstName != null ? firstName.concat(" ").concat(lastName) : "";
+    }
+}
