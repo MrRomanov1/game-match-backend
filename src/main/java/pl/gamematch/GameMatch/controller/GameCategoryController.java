a05ec87c8f9993dd1833face286cc1ea21a94d79
Piotr <piotr.romanczak@gmail.com>
2021-11-21 18:13:55 +0100

    GAM-22 refactored GameCategoryController

--- src/main/java/pl/gamematch/GameMatch/controller/GameCategoryController.java
+++ src/main/java/pl/gamematch/GameMatch/controller/GameCategoryController.java
@@ -2,14 +2,10 @@ package pl.gamematch.GameMatch.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.gamematch.GameMatch.dao.GameCategoryRepository;
 import pl.gamematch.GameMatch.model.game.GameCategory;
 import pl.gamematch.GameMatch.service.GameCategoryService;
-import pl.gamematch.GameMatch.utils.GameCategoryUtils;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by Piotr Romanczak on 12-11-2021
@@ -19,28 +15,18 @@ import java.util.Map;
 public class GameCategoryController {
 
     private GameCategoryService gameCategoryService;
-    private GameCategoryRepository gameCategoryRepository;
 
-    public GameCategoryController(GameCategoryService gameCategoryService, GameCategoryRepository gameCategoryRepository) {
+    public GameCategoryController(GameCategoryService gameCategoryService) {
         this.gameCategoryService = gameCategoryService;
-        this.gameCategoryRepository = gameCategoryRepository;
     }
 
     /**
      * Created by Piotr Romanczak on 12-11-2021
-     * Description: this method returns List of all games
+     * Description: this method returns List of all gameCategories
      * @return List<Game>
      */
-    @GetMapping("/game-categories/all")
-    public List<GameCategory> getAllGameCategories() {
-        List<GameCategory> unorderedGameCategoryList = gameCategoryRepository.findAll();
-
-        Map<GameCategory, Double> gameCategoryByRating = new HashMap<>();
-
-        for (GameCategory category : unorderedGameCategoryList) {
-            gameCategoryByRating.put(category, category.calculateCategoryRating());
-        }
-
-        return GameCategoryUtils.sortCategoriesByRatings(gameCategoryByRating);
+    @GetMapping("/game-categories")
+    public List<GameCategory> getAllGameCategoriesSortedByRatings() {
+        return gameCategoryService.getAllGameCategoriesSortedByRatings();
     }
 }
